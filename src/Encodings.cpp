30556914704b371dbe243d56b413396a9c43f338
larsbengel <77848869+larsbengel@users.noreply.github.com>
2024-09-24 14:24:44 +0200

    Refactoring (#2)
    * refactored reduct
    
    * refactored encoding

--- src/Encodings.cpp
+++ src/Encodings.cpp
@@ -1,21 +1,21 @@
 #include "Encodings.h"
 
 
-namespace Encodings { // TODO create active_arguments in encoding ?
+namespace Encodings {
 	void admissible_nonempty(AF & af, const IterableBitSet & active_arguments, SAT_Solver & solver) {
-		std::vector<int32_t> non_empty_clause;
-		for (const uint32_t & arg : active_arguments._array) {
-			non_empty_clause.push_back(af.accepted_var(arg));
-			solver.add_clause_2(-af.accepted_var(arg), -af.rejected_var(arg));
+		std::vector<int32_t> non_empty_clause(active_arguments._array.size());
+		for (size_t i = 0; i < active_arguments._array.size(); i++) {
+			non_empty_clause[i] = af.accepted_var(active_arguments._array[i]);
+			solver.add_clause_2(-af.accepted_var(active_arguments._array[i]), -af.rejected_var(active_arguments._array[i]));
 
-			std::vector<int32_t> out_clause;
-			for (size_t j = 0; j < af.attackers[arg].size(); j++) {
-				if (active_arguments._bitset[af.attackers[arg][j]]) {
-					solver.add_clause_2(-af.accepted_var(arg), af.rejected_var(af.attackers[arg][j]));
-					out_clause.push_back(af.accepted_var(af.attackers[arg][j]));
+			std::vector<int32_t> out_clause(af.attackers[active_arguments._array[i]].size()+1);
+			for (size_t j = 0; j < af.attackers[active_arguments._array[i]].size(); j++) {
+				if (active_arguments._bitset[af.attackers[active_arguments._array[i]][j]]) {
+					solver.add_clause_2(-af.accepted_var(active_arguments._array[i]), af.rejected_var(af.attackers[active_arguments._array[i]][j]));
+					out_clause[j] = af.accepted_var(af.attackers[active_arguments._array[i]][j]);
 				}
 			}
-			out_clause.push_back(-af.rejected_var(arg));
+			out_clause[af.attackers[active_arguments._array[i]].size()] = -af.rejected_var(active_arguments._array[i]);
 			solver.add_clause(out_clause);
 		}
 		solver.add_clause(non_empty_clause);
