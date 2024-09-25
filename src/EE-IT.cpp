23ca188a7da653fc9239c1b1b1a4cfb5296fbd24
Lars Bengel <lars.bengel@fernuni-hagen.de>
2024-09-25 10:36:57 +0200

    optimized ee-it

--- src/EE-IT.cpp
+++ src/EE-IT.cpp
@@ -2,6 +2,7 @@
 
 
 namespace Algorithms {
+    /* Enumerates initial sets of the argumentation framework */
     std::vector<std::vector<uint32_t>> enumerate_initial(AF & af, const IterableBitSet & active_arguments) {
         if (active_arguments._array.empty()) {
             return {};
@@ -9,22 +10,27 @@ namespace Algorithms {
 
         std::vector<std::vector<uint32_t>> result;
         std::vector<uint32_t> extension;
-        std::vector<int32_t> complement_clause;
-        complement_clause.reserve(active_arguments._array.size());
+        //std::vector<int32_t> complement_clause;
+        //complement_clause.reserve(active_arguments._array.size());
         std::vector<int32_t> minimization_clause;
         minimization_clause.reserve(active_arguments._array.size());
 
+        // Create encoding for non-empty admissible sets
         SAT_Solver solver = SAT_Solver(af.args);
         Encodings::admissible_nonempty(af, active_arguments, solver);
 
-        while (true) {
+        
+        while (true) { // iteratively compute models for the encoding
             bool found_extension = false;
-            while (true) {
+            while (true) { // Iteratively minimize the found model
                 int sat = solver.solve();
                 if (sat == UNSAT_V) break;
 
+                // add clause that ensures at least one accepted argument of the found model must not be accepted
+                // ensure that no new argument may be accepted via temporary assumptions
                 found_extension = true;
                 minimization_clause.clear();
+                minimization_clause.reserve(active_arguments._array.size());
                 for (const uint32_t arg : active_arguments._array) {
                     if (solver.model[arg]) {
                         minimization_clause.push_back(-af.accepted_var(arg));
@@ -32,22 +38,21 @@ namespace Algorithms {
                         solver.assume(-af.accepted_var(arg));
                     }
                 }
-                //if (minimization_clause.size() == 1) {
-                //    break;
-                //}
                 solver.add_clause(minimization_clause);
             }
-            if (found_extension) {
+            if (found_extension) { // if an extension has been found and minimized, add corresponding extension to result and add a complement clause to ensure it is not found again
                 extension.clear();
-                complement_clause.clear();
+                extension.reserve(active_arguments._array.size());
+                //complement_clause.clear();
+                //complement_clause.reserve(active_arguments._array.size());
                 for(const uint32_t & arg : active_arguments._array) {
                     if (solver.model[arg]) {
                         extension.push_back(arg);
-                        complement_clause.push_back(-af.accepted_var(arg));
+                        //complement_clause.push_back(-af.accepted_var(arg)); // TODO is this even needed ?
                     }
                 }
                 result.push_back(extension);
-                solver.add_clause(complement_clause);
+                //solver.add_clause(complement_clause);
             } else {
                 break;
             }
